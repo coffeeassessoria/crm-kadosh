96e54c8105d8b016adef7b42123955cbfbf3242a
Vinicius Arenales <arenales.vinicius@gmail.com>
2026-06-26 17:51:01 -0400

    fix: aumenta limite do body parser para 50mb (webhooks com áudio/imagem)

--- agente-kadu/src/server.ts
+++ agente-kadu/src/server.ts
@@ -7,7 +7,7 @@ import { startBriefingScheduler } from './queue/briefingScheduler';
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.use(healthRouter);
 app.use(whatsappRouter);
